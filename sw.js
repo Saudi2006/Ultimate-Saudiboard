a8b672f66d0382260fa0d577d9509b55b8c55877
Saudi2006 <167813849+Saudi2006@users.noreply.github.com>
2025-10-30 22:13:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('sboard-v1').then(c=>c.addAll(['saudiboard.html','manifest.json']))) ;
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
